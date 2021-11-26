d59a3a671a51d009253b1b7f04d9b88fc1184aa2
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2021-11-26 02:23:18 -0600

    Agregar archivos de proyecto.

---
+++ Caamtroh/Security/SessionPersister.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Web;
+
+namespace Caamtroh.Security
+{
+    public static class SessionPersister
+    {
+
+
+        public static string GetClaimInfo(string nameIdentifier)
+        {
+            ClaimsPrincipal Principal = Thread.CurrentPrincipal as ClaimsPrincipal;
+            var identificador =
+                Principal.Claims.FirstOrDefault(p => p.Type == nameIdentifier);
+            if (identificador != null)
+                return identificador.Value;
+            else
+                return "";
+        }
+    }
+}
