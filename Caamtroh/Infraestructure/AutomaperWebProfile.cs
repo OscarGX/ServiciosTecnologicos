a1691ec36c7c2af8e1b1e2c734a4616740634336
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2021-11-30 23:49:05 -0600

    Se agrego la clase automapper y la conversion de dominios a view models

---
+++ Caamtroh/Infraestructure/AutomaperWebProfile.cs
@@ -0,0 +1,19 @@
+﻿using Caamtroh.Domain;
+using Caamtroh.ViewModels;
+
+namespace Caamtroh.Infraestructure
+{
+    public class AutomaperWebProfile: AutoMapper.Profile
+    {
+        public AutomaperWebProfile()
+        {
+            //entidad AspNetRoles
+            CreateMap<AspNetRolesDomainModel,AspNetRolesVM>();
+            CreateMap<AspNetRolesVM, AspNetRolesDomainModel>();
+
+            //entidad AspNetUsers
+            CreateMap<AspNetUsersDomainModel, AspNetUsersVM>();
+            CreateMap<AspNetUsersVM, AspNetUsersDomainModel>();
+        }
+    }
+}
