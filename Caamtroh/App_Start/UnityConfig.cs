39189afa0e48032cc818bb503269bfd28403e95e
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2021-12-01 01:38:15 -0600

    Se agrego el unitycontainer para  trabajar con las referencias  de inyeccion de dependencias.

---
+++ Caamtroh/App_Start/UnityConfig.cs
@@ -0,0 +1,20 @@
+using Incubadora.Business;
+using Incubadora.Business.Interface;
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+
+namespace Incubadora
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            container.RegisterType<IAspNetRolesBusiness, AspNetRolesBusiness>();
+            
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
